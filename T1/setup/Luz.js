0d778e97518d3f9cf67cab2656f8164b217e4151
ketleenanne <ketleenanne.faria@gmail.com>
2025-12-03 21:38:26 -0300

    Atualiza configurações de sombra: aumenta o tamanho do mapa de sombra e ajusta a posição da luz principal

--- T1/setup/Luz.js
+++ T1/setup/Luz.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
 
 // A luz principal -> projetar sombra em TODOS os elementos
-const SHADOW_MAP_SIZE = 4096; // Tamanho do mapa de sombra
-const SHADOW_CAM_SIZE = 80; // Alcance da câmera de sombra
-const LIGHT_ANGLE_Y = -110 * (Math.PI / 180); // Ângulo para sombra não alongada
-const LIGHT_HEIGHT = 30; // Altura da luz
+// Quanto maior, melhor a qualidade da sombra, mas mais pesado o processamento
+const SHADOW_MAP_SIZE = 8192;
+const SHADOW_CAM_SIZE = 50; // Alcance da câmera de sombra
+const LIGHT_ANGLE_Y = -30 * (Math.PI / 180); // Ângulo para sombra não alongada
+const LIGHT_HEIGHT = 40; // Altura da luz
 const LIGHT_INTENSITY = 2.5; // Intensidade
 const LIGHT_COLOR = 0xffffff; //"rgb(255, 255, 255)";
 const AMBIENT_LIGHT_INTENSITY = 0.1; // Intensidade da luz ambiente
@@ -59,8 +60,8 @@ export function atualizarLuz(veiculo) {
   // Translação (Posição)
   // A luz acompanha a posição X e Z do carro.
   // A altura (Y) permanece constante.
-  luzPrincipal.position.x = veiculo.position.x + Math.sin(LIGHT_ANGLE_Y) * 20;
-  luzPrincipal.position.z = veiculo.position.z + Math.cos(LIGHT_ANGLE_Y) * 20;
+  luzPrincipal.position.x = veiculo.position.x + Math.sin(LIGHT_ANGLE_Y) * 110;
+  luzPrincipal.position.z = veiculo.position.z + Math.cos(LIGHT_ANGLE_Y) * 110;
   luzPrincipal.position.y = LIGHT_HEIGHT; // Altura fixa
 
   // Rotação/Direção da Sombra
