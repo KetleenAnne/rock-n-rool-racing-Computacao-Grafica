07f951bcc7a04f91b2d5c88696f0441ebd9c56c9
Samuel Reis <sreis5447@gmail.com>
2025-12-07 13:24:44 -0300

    Aumentando checkpoints pistas 2 e 3

--- T1/jogo/ConfigCheckpoints.js
+++ T1/jogo/ConfigCheckpoints.js
@@ -16,8 +16,11 @@ export const CHECKPOINTS_PISTA1 = [
 export const CHECKPOINTS_PISTA2 = [ //(30,70)
   { poste1: { x: -78, z: 82 }, poste2: { x: -78, z: 58 } },          // CP1 - Curva Sul esquerda inferior - Saida da Largada
   { poste1: { x: -102, z: -117 }, poste2: { x: -78, z: -117 } },     // CP2 - Curva Esquerda Superior
-  { poste1: { x: -3, z: -40 }, poste2: { x: 23, z: -40 } },          // CP3 - Antes Curva L
-  { poste1: { x: 70, z: 83 }, poste2: { x: 70, z: 57 } },            // CP4 - Antes Linha de Chegada
+  { poste1: { x: -3, z: -142 }, poste2: { x: -3, z: -117 } },        // CP3 - Curva Entrando no L 
+  { poste1: { x: -3, z: -40 }, poste2: { x: 23, z: -40 } },          // CP4 - Antes Curva L
+  { poste1: { x: 80, z: -22 }, poste2: { x: 79, z: 3 } },            // CP5 - Fim Curva L
+  { poste1: { x: 102, z: 57 }, poste2: { x: 79, z: 57 } },           // CP6 - Antes Curva Final
+  { poste1: { x: 70, z: 83 }, poste2: { x: 70, z: 57 } },            // CP7 - Antes Linha de Chegada
 ];
 
 // ========== PISTA 3 - DOIS QUADRADOS ==========
@@ -25,8 +28,10 @@ export const CHECKPOINTS_PISTA2 = [ //(30,70)
 export const CHECKPOINTS_PISTA3 = [
   { poste1: { x: -75, z: 111 }, poste2: { x: -75, z: 89 } },             // CP1 - Curva Sul esquerda inferior - Saida da Largada
   { poste1: { x: -102, z: -285 }, poste2: { x: -78, z: -285 } },         // CP2 - Direita Superior do segundo quadrado
-  { poste1: { x: -257, z: -120 }, poste2: { x: -283, z: -120 } },        // CP3 - Esquerda Inferior do segundo quadrado
-  { poste1: { x: 40, z: -112 }, poste2: { x: 40, z: -88 } },             // CP4 - Volta pro primeiro
+  { poste1: { x: -257, z: -285 }, poste2: { x: -257, z: -312 } },        // CP3 - Esquerda Superior do segundo quadrado
+  { poste1: { x: -257, z: -120 }, poste2: { x: -283, z: -120 } },        // CP4 - Esquerda Inferior do segundo quadrado
+  { poste1: { x: 77, z: -112 }, poste2: { x: 77, z: -88 } },             // CP5 - Volta pro primeiro
+  { poste1: { x: 105, z: 88 }, poste2: { x: 77, z: 88 } },               // CP6 - Curva Final
 ];
 
 // Função helper para adicionar checkpoints facilmente
