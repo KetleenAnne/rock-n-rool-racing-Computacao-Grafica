a4ba3a9854a27f7aeecc00dc314cfc7831be9287
KelteenAnne <ketleenanne.faria@gmail.com>
2026-01-25 19:59:28 -0300

    Atualiza texturas e remove classe Veiculo.js

--- T2/setup/Scene.js
+++ T2/setup/Scene.js
@@ -1,21 +1,14 @@
 import * as THREE from "three";
-import {
-  initDefaultBasicLight,
-  createGroundPlaneXZ,
-} from "../../libs/util/util.js";
-import { setDefaultMaterial } from "../../libs/util/util.js";
 import { criarLuzes } from "./Luz.js";
 
 let axesHelper = null;
 
 export function setupScene(scene) {
-  // let corCeu = "skyblue";
-  // initDefaultBasicLight(scene);
-  criarLuzes(scene); //  novo sistema de luz
+  criarLuzes(scene);
 
   //carregar o ceu
   const loader = new THREE.TextureLoader();
-  const caminhoCeu = "assets/texturas/sky/ceu3.jpg";
+  const caminhoCeu = "assets/texturas/sky/ceu2.jpg";
 
   const textureEquirec = loader.load(caminhoCeu);
   // diz ao Three.js que a imagem é uma esfera 360 graus
@@ -24,4 +17,8 @@ export function setupScene(scene) {
 
   //define como fundo
   scene.background = textureEquirec;
+
+  //  valor negativo para inclinar o fundo "para trás"
+  // faz com que a imagem pareça mais baixa na tela
+  scene.backgroundRotation.x = THREE.MathUtils.degToRad(-25);
 }
